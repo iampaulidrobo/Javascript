b2b9df1f4128cfedce164d06c63ab1eedf627f1d
Piyush Tailor <piyushtailor4@gmail.com>
2024-08-16 09:11:30 +0530

    covered for  html

---
+++ myscript.js
@@ -0,0 +1,6 @@
+function myFunctioninscript() {
+    document.getElementById("demo").innerHTML = "myFunctioninscript changed.";
+  }
+
+console.log("Hello World! I Don't Give a Bug");
+// myFunctioninscript()
